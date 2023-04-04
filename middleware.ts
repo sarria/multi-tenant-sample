b14edc96d3eed45e747d5ad7318cb113a978817e
sarria <juan@townsquaredigital.com>
2023-04-03 20:40:36 -0400

    Testing vercel domain

--- middleware.ts
+++ middleware.ts
@@ -25,20 +25,23 @@ export default async function middleware(req: NextRequest) {
   const path = url.pathname;
 
   // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.vercel.pub");
-  }
+  // if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
+  //   return NextResponse.redirect("https://demo.vercel.pub");
+  // }
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
       in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
       still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
-  const currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname
-          .replace(`.vercel.pub`, "")
-          .replace(`.platformize.vercel.app`, "")
-      : hostname.replace(`.localhost:3000`, "");
+  // const currentHost =
+  //   process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+  //     ? hostname
+  //         .replace(`.vercel.pub`, "")
+  //         .replace(`.platformize.vercel.app`, "")
+  //     : hostname.replace(`.localhost:3000`, "");
+
+  const currentHost = hostname.replace(`.localhost:3000`, "").replace(`.vercel.app`, "");
+
   // rewrites for app pages
   if (currentHost === "app") {
     const session = await getToken({
